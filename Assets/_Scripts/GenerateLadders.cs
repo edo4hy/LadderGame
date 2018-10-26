8d0130b987aee3bdea63fefd89042cdd911ea5be
Obree, Edward <edward.obree@cadentgas.com>
2018-10-26 10:22:34 +0100

    initial commit

---
+++ Assets/_Scripts/GenerateLadders.cs
@@ -0,0 +1,117 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GenerateLadders : MonoBehaviour {
+
+    public int xLadder;
+    public int zLadder;
+
+    public GameObject ladderT;
+    public GameObject ladderR;
+    public GameObject ladderC;
+    public GameObject ladderS;
+
+    public Material blue;
+    public Material red;
+
+    public GameObject ladderStore;
+
+    GameObject[] ladders = new GameObject[4];
+
+    float ladderStep = 3.2f;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        ladders[0] = ladderT;
+        ladders[1] = ladderR;
+        ladders[2] = ladderC;
+        ladders[3] = ladderS;
+
+		for(int i = 0; i < zLadder; i++)
+        {
+            for(int j = 0; j < xLadder; j++)
+            {
+                Debug.Log("creating new ladder");
+                GameObject newLadder = Instantiate(ladders[SelectLadder()], new Vector3(ladderStep * j, 0, ladderStep * i), Quaternion.Euler(SelectRotation()));
+                newLadder.GetComponent<RotateLadder>().direction = SelectDirection();
+                if(newLadder.GetComponent<RotateLadder>().direction == Enums.Direction.clockwise)
+                {
+                    newLadder.transform.GetChild(2).gameObject.GetComponentInChildren<MeshRenderer>().material = blue;
+                }
+                else
+                {
+                    newLadder.transform.GetChild(2).gameObject.GetComponentInChildren<MeshRenderer>().material = red;
+
+                }
+                newLadder.transform.parent = ladderStore.transform;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    int SelectLadder()
+    {
+        float select = Random.Range(0, 10);
+       if(select > 8)
+        {
+            return 3;
+        }else if(select > 6)
+        {
+            return 2;
+        }
+        else if( select > 3)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    Vector3 SelectRotation()
+    {
+        float select = Random.Range(0, 4);
+        if(select > 3)
+        {
+            return new Vector3(0, 0, 0);
+        }
+        else if(select > 2)
+        {
+            return new Vector3(0, 90, 0);
+        }
+        else if(select > 1)
+        {
+            return new Vector3(0, 180, 0);
+        }
+        else
+        {
+            return new Vector3(0, 270, 0);
+        }
+    }
+
+    Enums.Direction SelectDirection()
+    {
+        float select = Random.Range(0, 2);
+        Debug.Log(select);
+        if(select > 0.5)
+        {
+            return Enums.Direction.antiClockwise;
+        }
+        else
+        {
+            return Enums.Direction.clockwise;
+        }
+    }
+}
+ 
---
+++ Assets/_Scripts/RotateLadder.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotateLadder : MonoBehaviour {
+
+    bool clickSetNewRotation;
+    float degreeCount;
+
+
+    public Enums.Direction direction;
+
+    float rotationDegree;
+    public float speed;
+
+    Vector3 newDirection;
+    Vector3 newRotation;
+
+    bool canNewRotate;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (direction == Enums.Direction.clockwise)
+        {
+            rotationDegree = 90;
+        }
+        else
+        {
+            rotationDegree = -90;
+        }
+
+        speed = 1;
+
+        newRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (CheckEqualRotation(transform.rotation.eulerAngles, newRotation))
+        {
+            canNewRotate = true;
+        }
+        else
+        {
+            canNewRotate = false;
+            clickSetNewRotation = false;
+        }
+
+        if (clickSetNewRotation && canNewRotate)
+        {
+            canNewRotate = false;
+            Debug.Log("setting new value");
+            newRotation = new Vector3(0, this.transform.rotation.eulerAngles.y + rotationDegree, 0);
+
+            clickSetNewRotation = false;
+        }
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(newRotation), speed);
+
+    }
+
+    /*
+    * On mouse click on the collider of the object - 
+    * check that gameObject is a Ladder - 
+    * Try and set new rotation
+    */
+    private void OnMouseDown()
+    {
+        Debug.Log("Click");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Ladder")
+            {
+                clickSetNewRotation = true;
+            }
+            else
+            {
+                Debug.Log("This isn't a Player");
+            }
+        }
+    }
+
+    private Vector3 NormalizeRotationEulur(Vector3 rotation)
+    {
+        Vector3 start = rotation;
+        if(rotation.y == -90f)
+        {
+            rotation.y = 270f;
+        }
+        
+        if(rotation.y == 360f)
+        {
+            Debug.Log("reset");
+            rotation.y = 0.0f;
+        }
+
+        Debug.Log(rotation + "  " + start);
+
+        return Vector3.Normalize(rotation);
+    }
+
+    bool CheckEqualRotation(Vector3 currentRotation, Vector3 targetRotation)
+    {
+        if(currentRotation.y == targetRotation.y)
+        {
+            return true;
+        }
+        if(currentRotation.y == 360 && (int)targetRotation.y == 0)
+        {
+            return true;
+        }
+        if((int)currentRotation.y == 0 && targetRotation.y == 360)
+        {
+            return true;
+        }
+        if(currentRotation.y == -90 && targetRotation.y == 270)
+        {
+            return true;
+        }
+        if(currentRotation.y == 270 && targetRotation.y == -90)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
